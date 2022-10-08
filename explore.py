9bbe64db1d33870b9f6aed635f1a52f5d421367a
KacperChmura1 <112932143+KacperChmura1@users.noreply.github.com>
2022-10-08 14:23:31 +0200

    add plots to explore page

--- explore.py
+++ explore.py
@@ -2,4 +2,14 @@ import streamlit as st
 import pandas as pd
 df_dum = pd.read_csv("df_app.csv")
 def explore(df_dum):
-    st.markdown("Comming soon")
+    st.title("Most Populars Brands in Poland")
+    st.image("Plots/output1.png")
+    st.title("Most Populars Models in Poland")
+    st.image("Plots/output2.png")
+    st.title("Most Populars type of fuel in Poland")
+    st.image("Plots/output3.png")
+    st.title("Most Populars price range in Poland")
+    st.image("Plots/output4.png")
+    st.title("Most Populars car color in Poland")
+    st.image("Plots/output5.png")
+      
