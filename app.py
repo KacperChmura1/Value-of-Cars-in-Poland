612ba21a733e3c919948f15c34217d676fc4bfc6
KacperChmura1 <112932143+KacperChmura1@users.noreply.github.com>
2022-10-14 20:36:57 +0200

    Develop application with predict model which could predict car value with features

--- app.py
+++ app.py
@@ -1,43 +1,14 @@
 
+from enum import unique
 import streamlit as st
 from tensorflow.keras.models import load_model
 import pandas as pd
 from pickle import load
-model = load_model("Model/model_app.h5")
-df_dum = pd.read_csv("df_app.csv")
-#Brands(to delete probably)
-brands_10k = {'Opel', 'Audi', 'BMW', 'Volkswagen', 'Škoda', 'Ford', 'Renault',
-       'Mercedes-Benz', 'Toyota', 'Seat', 'Nissan', 'Kia', 'Hyundai',
-       'Peugeot', 'Mazda', 'Honda', 'Citroën', 'Fiat', 'Volvo', 'Dacia',
-       'Suzuki', 'Mitsubishi', 'Jeep', 'MINI', 'Saab', 'Alfa Romeo',
-       'Chevrolet', 'Subaru', 'Jaguar', 'Lexus', 'Chrysler', 'Smart',
-       'Land Rover'}
-brands_25k = {'Opel', 'Audi', 'BMW', 'Volkswagen', 'Škoda', 'Ford', 'Renault',
-       'Mercedes-Benz', 'Toyota', 'Seat', 'Nissan', 'Kia', 'Hyundai',
-       'Peugeot', 'Mazda', 'Honda', 'Citroën', 'Fiat', 'Volvo', 'Dacia',
-       'Suzuki', 'Mitsubishi', 'Abarth', 'Jeep', 'MINI', 'Saab',
-       'Alfa Romeo', 'Chevrolet', 'Subaru', 'Porsche', 'Jaguar', 'Lexus',
-       'Chrysler', 'Smart', 'Land Rover'}
-brands_50k = {'Opel', 'Audi', 'BMW', 'Volkswagen', 'Škoda', 'Ford', 'Renault',
-       'Mercedes-Benz', 'Toyota', 'Seat', 'Nissan', 'Kia', 'Hyundai',
-       'Peugeot', 'Mazda', 'Honda', 'Citroën', 'Fiat', 'Volvo', 'Dacia',
-       'Suzuki', 'Abarth', 'Mitsubishi', 'Jeep', 'MINI', 'Saab',
-       'Alfa Romeo', 'Chevrolet', 'Subaru', 'Porsche', 'Jaguar', 'Lexus',
-       'Chrysler', 'Smart', 'Land Rover', 'Dodge'}
+import numpy as np
+from datetime import datetime
+model = load_model("Model/model_with_features_under_200k.h5")
+df2 = pd.read_csv("Data/app.csv")
 
-brands_100k = {'Opel', 'Audi', 'BMW', 'Volkswagen', 'Škoda', 'Ford', 'Renault',
-       'Mercedes-Benz', 'Toyota', 'Seat', 'Nissan', 'Kia', 'Hyundai',
-       'Peugeot', 'Mazda', 'Honda', 'Citroën', 'Fiat', 'Volvo', 'Dacia',
-       'Suzuki', 'Abarth', 'Mitsubishi', 'Jeep', 'MINI', 'Saab',
-       'Chevrolet', 'Subaru', 'Alfa Romeo', 'Porsche', 'Jaguar', 'Lexus',
-       'Chrysler', 'Smart', 'Land Rover', 'Dodge'}
-
-brands_200k = {'Opel', 'Audi', 'BMW', 'Volkswagen', 'Škoda', 'Ford', 'Renault',
-       'Mercedes-Benz', 'Seat', 'Cupra', 'Toyota', 'Nissan', 'Kia',
-       'Hyundai', 'Peugeot', 'Mazda', 'Honda', 'Citroën', 'Volvo',
-       'Mitsubishi', 'Jeep', 'Suzuki', 'MINI', 'Subaru', 'Alfa Romeo',
-       'Porsche', 'Jaguar', 'Lexus', 'Fiat', 'Chevrolet', 'Land Rover',
-       'Chrysler', 'Dodge'}
 #Models
 Opel = ['Astra', 'Insignia', 'Corsa', 'Zafira', 'Meriva', 'Vectra',
        'Mokka', 'Vivaro', 'Grandland X', 'Crossland X', 'Combo', 'Signum',
@@ -122,13 +93,18 @@ production_year = {2015, 2006, 2010, 2007, 2008, 2005, 2013, 2011, 2002, 2018, 2
        1990, 1980, 1988, 1989, 1982, 1985, 1983, 1986, 1978, 1977, 1979,
        1981, 1984, 1973, 1976, 1966, 1970, 1971, 1968, 1963, 1965, 1967,
        1964, 1972, 1969, 1961, 1974}
+unique_features = ['ABS','Electricfrontwindows','Driversairbag','Powersteering','ASR(tractioncontrol)',
+ 'Rearviewcamera','Heatedsidemirrors','CD','Electricallyadjustablemirrors','Passengersairbag',
+ 'Alarm','Bluetooth','Automaticairconditioning','Airbagprotectingtheknees','Centrallocking','Immobilizer','Factoryradio','Alloywheels','Rainsensor','On-boardcomputer','Multifunctionsteeringwheel','AUXsocket','Xenonlights','USBsocket','MP3','ESP(stabilizationofthetrack)','Frontsideairbags','Rearparkingsensors',
+ 'Isofix','Aircurtains','Tintedwindows','Daytimerunninglights','Rearsideairbags','Foglights','Twilightsensor','GPSnavigation','LEDlights','Manualairconditioning','Start-Stopsystem','Electrochromicrearviewmirror','Velorupholstery','Electrochromicsidemirrors','SDsocket','Dualzoneairconditioning','Adjustablesuspension','Panoramicroof','Sunroof','Frontparkingsensors','Heatedfrontseats','Leatherupholstery','Electricallyadjustableseats','Cruisecontrol','Parkingassistant','Speedlimiter','Heatedwindscreen',
+'Electricrearwindows','Shiftpaddles','Blindspotsensor','Aftermarketradio','DVDplayer','CDchanger','Auxiliaryheating','Heatedrearseats','Four-zoneairconditioning','TVtuner','Roofrails','Activecruisecontrol','Hook','Laneassistant','HUD(head-updisplay)']
 
 def model_selection(brand):
     for i in range(0,len(brands_all)):
        if brand == brands_all[i]:
             model_c = st.selectbox("Vehicle model", models_list[i])
             return model_c
-def predict_car_value(df_dum):
+def predict_car_value(df2):
        st.title("Car Value Predictor")
        page = 0
        #page = st.selectbox("Price Range", {"0-10k","10k-25k","25k-50k","50k-100k","100k-200k"})
@@ -142,29 +118,132 @@ def predict_car_value(df_dum):
        fuel_type = st.selectbox("Fuel Type", {'Diesel', 'Gasoline', 'Gasoline + LPG', 'Hybrid', 'Gasoline + CNG'})
        drive = st.selectbox("Drive", {'Front wheels', 'Rear wheels', '4x4 (permanent)','4x4 (attached automatically)', '4x4 (attached manually)'})
        transmission = st.selectbox("Transmission", {'Manual', 'Automatic'})
+       type = st.selectbox("Type", {'station_wagon', 'compact', 'sedan', 'SUV', 'city_cars', 'minivan',
+       'coupe', 'small_cars', 'convertible'})
        doors = st.selectbox("Doors Number", {5.,  3.,  2.,  4., 55.,  7.,  6.,  1.,  9.})
        colour = st.selectbox("Colour", {'red', 'green', 'black', 'silver', 'blue', 'gray', 'white','burgundy', 'other', 'beige', 'brown', 'golden', 'yellow','violet'})
-       features = 30
+       features2 = st.multiselect("Features",unique_features)
+       month = int(datetime.now().month)
+       to_day_year = int(datetime.now().year)
+       
+       x = 0
+       i = 0
+       y = []
+       for el in unique_features:
+              for i in range(0,len(features2)):
+                     if el in features2:
+                            x = 1
+                     else:
+                            x = 0
+              y.append(x)    
        ok = st.button("Calculate Price")
 
        if ok:
-              car_df = pd.DataFrame(data = [[0, condition, brand, model_c, year, millage_km, power_hp, displacement_cm3,fuel_type, drive, transmission, doors, colour, features]], columns =
-              ['Price', 'Condition', 'Vehicle_brand', 'Vehicle_model',
-              'Production_year', 'Mileage_km', 'Power_HP', 'Displacement_cm3',
-              'Fuel_type', 'Drive', 'Transmission', 'Doors_number', 'Colour',
-              'Features'] )
+              st.markdown(y)   
+              car_df = pd.DataFrame(data = [[0, condition, brand, model_c, year, millage_km, power_hp, displacement_cm3,fuel_type, drive, transmission,type, doors, colour,y[0],
+y[1],
+y[2],
+y[3],
+y[4],
+y[5],
+y[6],
+y[7],
+y[8],
+y[9],
+y[10],
+y[11],
+y[12],
+y[13],
+y[14],
+y[15],
+y[16],
+y[17],
+y[18],
+y[19],
+y[20],
+y[21],
+y[22],
+y[23],
+y[24],
+y[25],
+y[26],
+y[27],
+y[28],
+y[29],
+y[30],
+y[31],
+y[32],
+y[33],
+y[34],
+y[35],
+y[36],
+y[37],
+y[38],
+y[39],
+y[40],
+y[41],
+y[42],
+y[43],
+y[44],
+y[45],
+y[46],
+y[47],
+y[48],
+y[49],
+y[50],
+y[51],
+y[52],
+y[53],
+y[54],
+y[55],
+y[56],
+y[57],
+y[58],
+y[59],
+y[60],
+y[61],
+y[62],
+y[63],
+y[64],
+y[65],
+y[66],
+y[67],
+y[68],
+y[69],month,to_day_year]], columns = ['Price', 'Condition', 'Vehicle_brand', 'Vehicle_model',
+       'Production_year', 'Mileage_km', 'Power_HP', 'Displacement_cm3',
+       'Fuel_type', 'Drive', 'Transmission', 'Type', 'Doors_number', 'Colour',
+       'ABS', 'Electricfrontwindows', 'Driversairbag', 'Powersteering',
+       'ASR(tractioncontrol)', 'Rearviewcamera', 'Heatedsidemirrors', 'CD',
+       'Electricallyadjustablemirrors', 'Passengersairbag', 'Alarm',
+       'Bluetooth', 'Automaticairconditioning', 'Airbagprotectingtheknees',
+       'Centrallocking', 'Immobilizer', 'Factoryradio', 'Alloywheels',
+       'Rainsensor', 'On-boardcomputer', 'Multifunctionsteeringwheel',
+       'AUXsocket', 'Xenonlights', 'USBsocket', 'MP3',
+       'ESP(stabilizationofthetrack)', 'Frontsideairbags',
+       'Rearparkingsensors', 'Isofix', 'Aircurtains', 'Tintedwindows',
+       'Daytimerunninglights', 'Rearsideairbags', 'Foglights',
+       'Twilightsensor', 'GPSnavigation', 'LEDlights', 'Manualairconditioning',
+       'Start-Stopsystem', 'Electrochromicrearviewmirror', 'Velorupholstery',
+       'Electrochromicsidemirrors', 'SDsocket', 'Dualzoneairconditioning',
+       'Adjustablesuspension', 'Panoramicroof', 'Sunroof',
+       'Frontparkingsensors', 'Heatedfrontseats', 'Leatherupholstery',
+       'Electricallyadjustableseats', 'Cruisecontrol', 'Parkingassistant',
+       'Speedlimiter', 'Heatedwindscreen', 'Electricrearwindows',
+       'Shiftpaddles', 'Blindspotsensor', 'Aftermarketradio', 'DVDplayer',
+       'CDchanger', 'Auxiliaryheating', 'Heatedrearseats',
+       'Four-zoneairconditioning', 'TVtuner', 'Roofrails',
+       'Activecruisecontrol', 'Hook', 'Laneassistant', 'HUD(head-updisplay)',
+       'Month', 'Year'])
               #model.predic([[condition,brand,year,millage_km,pow]])
 
-              car_dum = pd.DataFrame(car_df)
-              car_dum = pd.get_dummies(car_dum)
-              df_dum = df_dum.append(car_dum).fillna(0)
-              
-              scaler = load(open('Scalers/scaler_app.pkl', 'rb'))
-              X_car = df_dum.drop(["Price","Unnamed: 0"], axis = 1)
+              #car_dum = pd.DataFrame(car_df)
+              df2 = df2.append(car_df).fillna(0)
+              car_dum = pd.get_dummies(df2,drop_first=True)
+              scaler = load(open('Scalers/scaler_with_features.pkl', 'rb'))
+              X_car = car_dum.drop(["Price",'Unnamed: 0'], axis = 1)
+              X_car = X_car.iloc[[1,-1]]
               # jeden feature za duzo
               to_pred = scaler.transform(X_car)
-              
-
               car_price = model.predict(to_pred)[-1]
               true_value = f'<p style="font-family:Courier; color:Green; font-size: 30px;">Predicted Value: {round(car_price[0])}PLN</p>'
               st.markdown(true_value, unsafe_allow_html=True)
--- menu.py
+++ menu.py
@@ -5,8 +5,9 @@ import pandas as pd
 #Pages choose
 page = st.sidebar.selectbox("Explore Or Predict", {"Predict price of your car","Explore some informations"})
 df_dum = pd.read_csv("df_app.csv")
+df2 = pd.read_csv("xxx.csv")
 if page == "Predict price of your car":
-    predict_car_value(df_dum)
+    predict_car_value(df2)
 elif page == "Explore some informations":
     explore(df_dum)
 
