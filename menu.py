767466c8486c4cd3733c96d17c2717065e076894
KacperChmura1 <112932143+KacperChmura1@users.noreply.github.com>
2022-10-24 14:53:25 +0200

    Streamlit bugfix

--- menu.py
+++ menu.py
@@ -1,11 +1,10 @@
 from app import predict_car_value
-from explore import explore
 import streamlit as st
 import pandas as pd
-#Loading Data
+# Loading Data
 df_dum = pd.read_csv("df_app.csv")
-df2 = pd.read_csv("Data/app.csv")
-#Select Page
+df2 = pd.read_csv("app.csv")
+# Select Page
 page = st.sidebar.selectbox("Explore Or Predict", {"Predict price of your car"})
 if page == "Predict price of your car":
     predict_car_value(df2)
