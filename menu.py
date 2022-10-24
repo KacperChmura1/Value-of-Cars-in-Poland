cec5b4f7650e0850f65ed30e8be22f1b9185e582
KacperChmura1 <112932143+KacperChmura1@users.noreply.github.com>
2022-10-24 15:17:39 +0200

    5-th streamlit bugfix

--- menu.py
+++ menu.py
@@ -3,7 +3,7 @@ import streamlit as st
 import pandas as pd
 # Loading Data
 df_dum = pd.read_csv("df_app.csv")
-df2 = pd.read_csv("C:/Users/kacpe/Desktop/programowanie/Programowanie_w_Python/Car_Value_Project/app.csv")
+df2 = pd.read_csv("app.csv")
 # Select Page
 page = st.sidebar.selectbox("Explore Or Predict", {"Predict price of your car"})
 if page == "Predict price of your car":
