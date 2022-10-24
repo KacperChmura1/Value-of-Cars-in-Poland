58e65b4d09644c34001824811d99957a903ea5f9
KacperChmura1 <112932143+KacperChmura1@users.noreply.github.com>
2022-10-24 14:42:41 +0200

    delete explore stuff

--- menu.py
+++ menu.py
@@ -6,11 +6,8 @@ import pandas as pd
 df_dum = pd.read_csv("df_app.csv")
 df2 = pd.read_csv("Data/app.csv")
 #Select Page
-page = st.sidebar.selectbox("Explore Or Predict", {"Predict price of your car","Explore some informations"})
+page = st.sidebar.selectbox("Explore Or Predict", {"Predict price of your car"})
 if page == "Predict price of your car":
     predict_car_value(df2)
-elif page == "Explore some informations":
-    explore(df_dum)
-
 
 
