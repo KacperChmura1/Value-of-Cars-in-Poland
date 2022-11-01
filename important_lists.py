a227d9e63d0b3a289962a42148b1e521f1117106
KacperChmura1 <112932143+KacperChmura1@users.noreply.github.com>
2022-11-01 14:59:02 +0100

    git change name and put all lists to another fail importatnt_lists.py

---
+++ important_lists.py
@@ -0,0 +1,114 @@
+Opel = ['Astra', 'Insignia', 'Corsa', 'Zafira', 'Meriva', 'Vectra',
+       'Mokka', 'Vivaro', 'Grandland X', 'Crossland X', 'Combo', 'Signum',
+       'Antara', 'Agila']
+Audi = ['A4', 'A6', 'A3', 'A5', 'Q5', 'A8', 'Q7', 'Q3', 'A7', 'A1',
+       'A6 Allroad', 'TT', 'Q2', 'A4 Allroad']
+BMW = ['Seria 3', 'Seria 5', 'Seria 1', 'X3', 'X1', 'X5', 'Seria 7',
+       'Seria 2', 'Seria 4', '3GT', 'Seria 6', 'X6', 'X2', 'X4']
+Volkswagen = ['Golf', 'Passat', 'Polo', 'Tiguan', 'Touran', 'Caddy', 'Golf Plus',
+       'Sharan', 'Jetta', 'up!', 'Arteon', 'Transporter', 'T-Roc',
+       'Touareg', 'T-Cross', 'Caravelle', 'CC', 'Multivan', 'Scirocco',
+       'New Beetle', 'Golf Sportsvan', 'Passat CC', 'Fox']
+Škoda = ['Octavia', 'Fabia', 'Superb', 'RAPID', 'Scala', 'Karoq', 'Citigo',
+       'Kodiaq', 'Roomster', 'Yeti']
+Ford = ['Focus', 'Mondeo', 'Fiesta', 'S-Max', 'Kuga', 'C-MAX', 'Galaxy',
+       'Mustang', 'Fusion', 'EcoSport', 'Puma', 'Grand C-MAX', 'EDGE',
+       'KA', 'B-MAX', 'Focus C-Max', 'Transit', 'Tourneo Custom',
+       'Ranger', 'Transit Custom']
+Renault = ['Megane', 'Clio', 'Scenic', 'Laguna', 'Captur', 'Grand Scenic',
+       'Kadjar', '5', 'Trafic', 'Espace', 'Talisman', 'Twingo', 'Modus',
+       'Kangoo', 'Koleos', 'Grand Espace']
+Mercedes_Benz = ['Klasa E', 'Klasa C', 'Klasa A', 'Klasa B', 'Klasa S', 'CLA', 'ML',
+       'GLA', 'CLK', 'SL', 'CLS', 'GLC', 'Vito', 'W124 (1984-1993)', 'GL',
+       'CL', 'GLK']
+Seat = ['Leon', 'Ibiza', 'Altea', 'Arona', 'Altea XL', 'Alhambra',
+       'Toledo', 'Ateca', 'Exeo']
+Cupra = ['Leon', 'Ateca']
+Toyota = ['Avensis', 'Yaris', 'Corolla', 'Auris', 'RAV4', 'C-HR', 'Aygo',
+       'Verso', 'Corolla Verso', 'Prius', 'Camry', 'Land Cruiser']
+Nissan = ['Qashqai', 'Juke', 'Micra', 'X-Trail', 'Note', 'Qashqai+2']
+Kia = ['Ceed', 'Sportage', 'Picanto', 'Rio', 'Venga', 'Optima', 'Stonic',
+       'XCeed', 'Sorento', 'Carens', "Pro_cee'd"]
+Hyundai = ['I30', 'Tucson', 'ix35', 'i20', 'i40', 'Kona', 'Santa Fe', 'i10',
+       'ix20', 'Elantra']
+Peugeot = ['308', '508', '3008', '208', '207', '5008', '407', '2008', '307',
+       'Partner', '206', 'Rifter', '301', '107']
+Mazda = ['6', '3', 'CX-5', '5', '2', 'CX-3', 'CX-30']
+Honda = ['Civic', 'CR-V', 'Accord', 'Jazz', 'HR-V']
+Citroën = ['C5', 'C3', 'C4 Picasso', 'C4', 'Berlingo', 'C4 Grand Picasso',
+       'C5 Aircross', 'C1', 'C3 Picasso', 'C3 Aircross', 'Xsara Picasso',
+       'C4 Cactus', 'DS3', 'C2', 'DS4', 'DS5']
+Volvo = ['XC 60', 'V40', 'V60', 'V50', 'S60', 'V70', 'XC 90', 'S80', 'S40',
+       'C30', 'XC 40', 'V90', 'S90', 'XC 70']
+Dacia = ['Duster', 'Sandero', 'Logan', 'Sandero Stepway', 'Lodgy', 'Dokker']
+Suzuki = ['Swift', 'Vitara', 'SX4', 'SX4 S-Cross', 'Grand Vitara', 'Ignis',
+       'Jimny']
+Abarth = ['500', 'Grande Punto']
+Mitsubishi = ['Outlander', 'ASX', 'Colt', 'Lancer', 'Space Star']
+Jeep = ['Grand Cherokee', 'Renegade', 'Compass', 'Cherokee', 'Wrangler']
+MINI = ['Cooper', 'Countryman', 'ONE', 'Cooper S', 'Clubman']
+Saab = ['9-3']
+Alfa_Romeo = ['159', 'Giulietta', 'Mito', 'Giulia']
+Chevrolet = ['Cruze', 'Aveo', 'Captiva', 'Camaro', 'Spark']
+Subaru = ['Forester', 'Legacy', 'Impreza', 'OUTBACK']
+Porsche = ['Cayenne']
+Jaguar = ['XF', 'XJ', 'XE']
+Lexus = ['IS', 'NX', 'RX', 'GS']
+Chrysler = ['Town & Country', 'Pacifica']
+Smart = ['Fortwo']
+Land_Rover = ['Range Rover Sport', 'Range Rover Evoque']
+Fiat = ['Tipo', '500', 'Panda', 'Grande Punto', 'Bravo', 'Punto', 'Doblo',
+       '500X', 'Punto Evo', 'Freemont', '500L', 'Sedici']
+Dodge = ['Challenger']
+
+
+brands_all = ['Opel', 'Audi', 'BMW', 'Volkswagen', 'Škoda', 'Ford', 'Renault',
+       'Mercedes-Benz', 'Toyota', 'Seat', 'Cupra', 'Nissan', 'Kia',
+       'Hyundai', 'Peugeot', 'Mazda', 'Honda', 'Citroën', 'Fiat', 'Volvo',
+       'Dacia', 'Suzuki', 'Abarth', 'Mitsubishi', 'Jeep', 'MINI', 'Saab',
+       'Alfa Romeo', 'Chevrolet', 'Subaru', 'Porsche', 'Jaguar', 'Lexus',
+       'Chrysler', 'Smart', 'Land Rover', 'Dodge']
+
+models_list = [Opel, Audi, BMW, Volkswagen, Škoda, Ford, Renault,
+       Mercedes_Benz, Toyota, Seat, Cupra, Nissan, Kia,
+       Hyundai, Peugeot, Mazda, Honda, Citroën, Fiat, Volvo,
+       Dacia, Suzuki, Abarth, Mitsubishi, Jeep, MINI, Saab,
+       Alfa_Romeo, Chevrolet, Subaru, Porsche, Jaguar, Lexus,
+       Chrysler, Smart, Land_Rover, Dodge]
+production_year = {2015, 2006, 2010, 2007, 2008, 2005, 2013, 2011, 2002, 2018, 2017,
+       2001, 2003, 2012, 2004, 2014, 2016, 1999, 2019, 2009, 2020, 1998,
+       2000, 1994, 1993, 2021, 1997, 1995, 1996, 1975, 1991, 1992, 1987,
+       1990, 1980, 1988, 1989, 1982, 1985, 1983, 1986, 1978, 1977, 1979,
+       1981, 1984, 1973, 1976, 1966, 1970, 1971, 1968, 1963, 1965, 1967,
+       1964, 1972, 1969, 1961, 1974}
+#Featues
+columns = ['Price', 'Condition', 'Vehicle_brand', 'Vehicle_model',
+       'Production_year', 'Mileage_km', 'Power_HP', 'Displacement_cm3',
+       'Fuel_type', 'Drive', 'Transmission', 'Type', 'Doors_number', 'Colour',
+       'ABS', 'Electricfrontwindows', 'Driversairbag', 'Powersteering',
+       'ASR(tractioncontrol)', 'Rearviewcamera', 'Heatedsidemirrors', 'CD',
+       'Electricallyadjustablemirrors', 'Passengersairbag', 'Alarm',
+       'Bluetooth', 'Automaticairconditioning', 'Airbagprotectingtheknees',
+       'Centrallocking', 'Immobilizer', 'Factoryradio', 'Alloywheels',
+       'Rainsensor', 'On-boardcomputer', 'Multifunctionsteeringwheel',
+       'AUXsocket', 'Xenonlights', 'USBsocket', 'MP3',
+       'ESP(stabilizationofthetrack)', 'Frontsideairbags',
+       'Rearparkingsensors', 'Isofix', 'Aircurtains', 'Tintedwindows',
+       'Daytimerunninglights', 'Rearsideairbags', 'Foglights',
+       'Twilightsensor', 'GPSnavigation', 'LEDlights', 'Manualairconditioning',
+       'Start-Stopsystem', 'Electrochromicrearviewmirror', 'Velorupholstery',
+       'Electrochromicsidemirrors', 'SDsocket', 'Dualzoneairconditioning',
+       'Adjustablesuspension', 'Panoramicroof', 'Sunroof',
+       'Frontparkingsensors', 'Heatedfrontseats', 'Leatherupholstery',
+       'Electricallyadjustableseats', 'Cruisecontrol', 'Parkingassistant',
+       'Speedlimiter', 'Heatedwindscreen', 'Electricrearwindows',
+       'Shiftpaddles', 'Blindspotsensor', 'Aftermarketradio', 'DVDplayer',
+       'CDchanger', 'Auxiliaryheating', 'Heatedrearseats',
+       'Four-zoneairconditioning', 'TVtuner', 'Roofrails',
+       'Activecruisecontrol', 'Hook', 'Laneassistant', 'HUD(head-updisplay)',
+       'Month', 'Year']
+unique_features = ['ABS','Electricfrontwindows','Driversairbag','Powersteering','ASR(tractioncontrol)',
+ 'Rearviewcamera','Heatedsidemirrors','CD','Electricallyadjustablemirrors','Passengersairbag',
+ 'Alarm','Bluetooth','Automaticairconditioning','Airbagprotectingtheknees','Centrallocking','Immobilizer','Factoryradio','Alloywheels','Rainsensor','On-boardcomputer','Multifunctionsteeringwheel','AUXsocket','Xenonlights','USBsocket','MP3','ESP(stabilizationofthetrack)','Frontsideairbags','Rearparkingsensors',
+ 'Isofix','Aircurtains','Tintedwindows','Daytimerunninglights','Rearsideairbags','Foglights','Twilightsensor','GPSnavigation','LEDlights','Manualairconditioning','Start-Stopsystem','Electrochromicrearviewmirror','Velorupholstery','Electrochromicsidemirrors','SDsocket','Dualzoneairconditioning','Adjustablesuspension','Panoramicroof','Sunroof','Frontparkingsensors','Heatedfrontseats','Leatherupholstery','Electricallyadjustableseats','Cruisecontrol','Parkingassistant','Speedlimiter','Heatedwindscreen',
+'Electricrearwindows','Shiftpaddles','Blindspotsensor','Aftermarketradio','DVDplayer','CDchanger','Auxiliaryheating','Heatedrearseats','Four-zoneairconditioning','TVtuner','Roofrails','Activecruisecontrol','Hook','Laneassistant','HUD(head-updisplay)']
--- menu.py
+++ menu.py
@@ -1,12 +1,14 @@
-from app import predict_car_value
+from ann import predict_car_value
 import streamlit as st
 import pandas as pd
 # Loading Data
 df_dum = pd.read_csv("df_app.csv")
 df2 = pd.read_csv("app.csv")
 # Select Page
-page = st.sidebar.selectbox("Explore Or Predict", {"Predict price of your car"})
-if page == "Predict price of your car":
+page = st.sidebar.selectbox("Explore Or Predict", {"ANN","ML"})
+if page == "ANN":
+    predict_car_value(df2)
+elif page == "ML":
     predict_car_value(df2)
 
 
